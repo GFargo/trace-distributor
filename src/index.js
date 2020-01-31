0ca712f26cf5eb2c1fd36d5f0ec52dede1a41404
Brian <brian.ludlam@gmail.com>
2020-01-31 16:23:34 -0500

    Bring over regulator dev env, and add core dependencies

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+/** 
+ * IE Polyfills
+ * @see https://github.com/facebook/create-react-app/tree/master/packages/react-app-polyfill
+ */
+// import 'react-app-polyfill/ie9'; // For IE 9-11 support
+// import 'react-app-polyfill/ie11'; // For IE 11 support
+
+
 import App from './App';
-import registerServiceWorker from './trace-core/registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
+
+require('dotenv').config();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
---
+++ src/styles/README.md
@@ -0,0 +1,15 @@
+# Styles
+
+
+
+### TailwindCSS
+
+**Note:** After making changes `tailwind.config.js` make sure to restart `npm start` process or manually run tailwind styles via `npm run build:style`.
+
+
+- https://tailwindcss.com/
+- https://nerdcave.com/tailwind-cheat-sheet
+
+
+**Plugins:**
+- https://github.com/benface/tailwindcss-transitions
