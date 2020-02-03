e9fcb8eb5e77dced66a0a8645c277ab5f9b002cb
Brian <brian.ludlam@gmail.com>
2020-02-03 12:21:13 -0500

    Added images fix back in for now to fix errors in lot states with images.

--- src/pages/DistributorLotDetail.js
+++ src/pages/DistributorLotDetail.js
@@ -3,7 +3,32 @@ import PropTypes from 'prop-types'
 import Pending from './PendingTrans'
 import Lot from '../core/src/components/Lots/Lot'
 
-const LotDetail = ({ lot }) => (!lot) ? <Pending /> : <Lot data={lot} />
+const fixImages = (images) => (
+  (!!images?.length) ? images.map((each) => 
+    !!each?.image && <a key={each.image.hash} href={each.image.url} target="_blank" rel="noopener noreferrer">
+      {each.caption || 'image'}
+    </a>) : null
+)
+
+const fixStateImages = (lot) => (!!lot?.stateDetails?.data?.images?.length) ? ({
+  ...lot,
+  stateDetails: {
+    ...lot.stateDetails,
+    data: {
+      ...lot.stateDetails.data,
+      images: fixImages(lot.stateDetails.data.images)
+    }
+  },
+  details: lot.details.map((state) => (!!state?.data?.images?.length) ? ({
+    ...state,
+    data: {
+      ...state.data,
+      images: fixImages(state.data.images)
+    }
+  }) : state)
+}) : lot
+
+const LotDetail = ({ lot }) => (!lot) ? <Pending /> : <Lot data={fixStateImages(lot)} />
 
 LotDetail.propTypes = {
   lot: PropTypes.object.isRequired
