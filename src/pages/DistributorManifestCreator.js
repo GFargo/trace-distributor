6593beb79773ef3e1e4da2c9b0c3c912ad4fb9c4
Brian <brian.ludlam@gmail.com>
2020-02-19 22:53:22 -0500

    Remove lot detail key value translations, moved to core.

--- src/pages/DistributorManifestCreator.js
+++ src/pages/DistributorManifestCreator.js
@@ -1,112 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { LotSectionKeys, transformValues, formatDateString } from '../core/src/components/Lots/LotStateSection/LotSectionHelpers'
+import { LotSectionKeys, transformValues } from '../core/src/components/Lots/LotStateSection/LotSectionHelpers'
 import Button from '../core/src/components/Elements/Button'
 
 const LOT_ENTRIES = [ 'name', 'address', 'totalSupply', 'created' ]
 const ORG_ENTRIES = [ 'name', 'domain', 'location' ]
 
-/* Merge with evisting in core eventually */
-const extLotSectionKeys = (key) => {
-  const newKey = LotSectionKeys(key)
-  if (newKey !== key) return newKey
-
-  const keys = {
-    "name": "Name",
-    "address": "Blockchain",
-    "created": "Created",
-    "totalSupply": "Total Supply",
-    "domain": "Web",
-    "location": "Address",
-    "seedOrCloneAmount": "Seed/Clone Amount",
-    "images": "Image",
-    "firstDateDrying": "First Drying",
-    "lastDateDrying": "Last Drying",
-    "averageHumidity": "Avg Humidity",
-    "averageTemperature": "Avg Temperature",
-    "dryingMethod": "Drying Method",
-    "potentialCBDProfile": "Potential CBD",
-    "potentialTHCProfile": "Potential THC",
-    "grindingMoisture": "Grinding Moisture",
-    "preExtractionMass": "Pre-Extraction Mass",
-    "postExtractionMass": "Post-Extraction Mass",
-    "wasteMaterialMass" : "Waste-Material Mass",
-    "preWinterizationMass" : "Pre-Winterization Mass",
-    "postWinterizationMass" : "Post-Winterization Mass",
-    "extractionEfficiency" : "Extraction Efficiency",
-    "winterizationEfficiency" : "Winterization Efficiency",
-    "additionalExtractionEfficiency" : "Additional Extraction Efficiency",
-  }
-  return keys[key] || key
-}
-
-/* Merge with evisting in core eventually */
-const extTransformValues = (key, value) => {
-  if (!key || !value) return ''
-
-  const newValue = transformValues(key, value)
-  if (newValue !== value) return newValue
-
-  switch (key) {
-    // stuff that is in grams
-    case "totalSupply":
-    case "sampleSize":
-    case "seedOrCloneAmount":
-    case "preExtractionMass":
-    case "postExtractionMass":
-    case "wasteMaterialMass":
-    case "preWinterizationMass":
-    case "postWinterizationMass":
-    case "preGrindingMass":
-    case "postGrindingMass":
-      return(value + "g");
-
-    // Test profile objects {"percentage": "20.20", "mass": "200"}
-    case "potentialCBDProfile":
-    case "potentialTHCProfile":
-      return(value["percentage"] + "% / " + value["mass"] + "g");
-
-    // temps
-    case "averageTemperature":
-    case "maxTemp":
-      return(value + String.fromCharCode(176));
-
-    // dates
-    case "lastDateDrying":
-    case "firstDateDrying":
-     return(formatDateString(value));
-
-    // percentages
-    case "averageHumidity":
-    case "grindingMoisture":
-    case "extractionEfficiency":
-    case "winterizationEfficiency":
-    case "additionalExtractionEfficiency":
-      return(value.toFixed(2) + "%");
-
-    case "images":
-      return ''/*(!value || !value[0]) ? '' : (
-        <a href={value[0].image.url} target="_blank" rel="noopener noreferrer">
-          {value[0].caption || 'image'}
-        </a>
-      )*/
-    case "productImage":
-      return ''/*
-        (!value) ? '' : (
-          <a href={value.url} target="_blank" rel="noopener noreferrer">
-            image
-          </a>
-        )*/
-    case "location":
-      return (!value["state"]) ? '' : 
-        (value["address1"] || '')+' '+(value["address2"] || '')+' '+value["state"]+' '+(value["zipcode"] || '')
-
-    default:
-      return(value)
-  }
-}
-
-/* Possibly move to core eventually */
 const transformStateValue = (state) => (
   (state === 'new') ? 'New' :
   (state === 'initial') ? 'Initial State' :
@@ -167,13 +66,13 @@ const SelectionItems = ({ lots, selection, onToggleSelection }) => {
         if (!!selection[lot.address+'-lot']) {
           LOT_ENTRIES.forEach((entry) => { 
             const key = lot.address+'-lot-'+entry
-            const value = !!entry && extTransformValues(entry, lot[entry])
+            const value = !!entry && transformValues(entry, lot[entry])
             if (!!value) {
               SelectionItems.push(
                 <CheckboxListItem 
                   key={key} 
                   id={key}
-                  label={extLotSectionKeys(entry)+': '+value}
+                  label={LotSectionKeys(entry)+': '+value}
                   indent={20}
                   isChecked={selection[key]}
                   isMeta={false}
@@ -197,13 +96,13 @@ const SelectionItems = ({ lots, selection, onToggleSelection }) => {
         if (!!selection[lot.address+'-org']) {
           ORG_ENTRIES.forEach((entry) => { 
             const key = lot.address+'-org-'+entry
-            const value = !!entry && extTransformValues(entry, lot.organization[entry])
+            const value = !!entry && transformValues(entry, lot.organization[entry])
             if (!!value) {
               SelectionItems.push(
                 <CheckboxListItem 
                   key={key} 
                   id={key}
-                  label={extLotSectionKeys(entry)+': '+value}
+                  label={LotSectionKeys(entry)+': '+value}
                   indent={20}
                   isChecked={selection[key]}
                   isMeta={false}
@@ -228,13 +127,13 @@ const SelectionItems = ({ lots, selection, onToggleSelection }) => {
           if (!!selection[lot.address] && !!selection[lot.address+'-'+lotState.state]) {
             Object.keys(lotState.data).forEach((entry) => {
               const key = lot.address+'-'+lotState.state+'-'+entry
-              const value = !!entry && extTransformValues(entry, lotState.data[entry])
+              const value = !!entry && transformValues(entry, lotState.data[entry])
               if (!!value) {
                 SelectionItems.push(
                   <CheckboxListItem 
                     key={key} 
                     id={key} 
-                    label={extLotSectionKeys(entry)+': '+value}       
+                    label={LotSectionKeys(entry)+': '+value}       
                     indent={20}
                     isChecked={selection[key]}          
                     isMeta={false}
@@ -257,7 +156,7 @@ const ManifestView = ({ lots, selection, onToggleSelection, onExportLotDataSelec
       <p className="h5">Create Bill of Goods</p>
       {!!Object.keys(selection).filter((each) => !!selection[each] && each.split('-').length === 3).length && (
         <Button 
-          type="link"
+          type="button"
           color="green"
           className="ml-6"
           onClickHandler={onExportLotDataSelection}
