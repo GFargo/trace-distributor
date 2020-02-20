01920fdec04ab948308a56f465f12016d8161f03
Brian <brian.ludlam@gmail.com>
2020-02-19 22:53:47 -0500

    Remove images fix, resolved in core.

--- src/pages/DistributorLotDetail.js
+++ src/pages/DistributorLotDetail.js
@@ -3,38 +3,7 @@ import PropTypes from 'prop-types'
 import Pending from '../core/src/components/Elements/Loader'
 import Lot from '../core/src/components/Lots/Lot'
 
-const translateImagesToAnchors = (images) => (
-  (!!images?.length) ? images.map((each) => 
-    !!each?.image && <a key={each.image.hash} href={each.image.url} target="_blank" rel="noopener noreferrer">
-      {each.caption || 'image'}
-    </a>) : null
-)
-/*
-const translateImagesToUrl = (images) => (
-  (!!images?.length) ? images.map((each) => !!each?.image && each.image.url) : null
-)
-*/
-const fixStateImages = (lot) => (!!lot?.stateDetails?.data) ? ({
-  ...lot,
-  stateDetails: {
-    ...lot.stateDetails,
-    data: {
-      ...lot.stateDetails.data,
-      images: (!!lot?.stateDetails?.data?.images) ? translateImagesToAnchors(lot.stateDetails.data.images) : undefined,
-      productImage: (!!lot?.stateDetails?.data?.productImage) ? translateImagesToAnchors([lot.stateDetails.data.productImage]) : undefined
-    }
-  },
-  details: lot.details.map((state) => (!!state?.data?.images || !!state?.data?.productImage) ? ({
-    ...state,
-    data: {
-      ...state.data,
-      images: (!!state?.data?.images) ? translateImagesToAnchors(state.data.images) : undefined,
-      productImage: (!!state?.data?.productImage) ? translateImagesToAnchors([state.data.productImage]) : undefined
-    }
-  }) : state)
-}) : lot
-
-const LotDetail = ({ lot }) => (!lot) ? <Pending /> : <Lot data={fixStateImages(lot)} />
+const LotDetail = ({ lot }) => (!lot) ? <Pending /> : <Lot data={lot} />
 
 LotDetail.propTypes = {
   lot: PropTypes.object.isRequired
