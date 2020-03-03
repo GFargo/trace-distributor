6dfa5eafb8875b4ec304574ff875ad47d37d7feb
Griffen Fargo <GhFargo@gmail.com>
2020-03-03 18:17:59 -0500

    Unifies styles on public landing page & login page

--- src/layouts/LandingLayout.js
+++ src/layouts/LandingLayout.js
@@ -4,9 +4,9 @@ import Footer from './Footer/Footer'
 import Header from './Header/Header'
 
 const LandingLayout = ({ invertColors = false, className, children }) => (
-  <div className="">
-    <div className="container-fluid">
-      <div className="flex flex-col min-h-screen min-w-screen px-2">
+  <div className="bg-gray-100">
+    <div className="container ml-12 mr-auto">
+      <div className="flex flex-col min-h-screen min-w-screen">
         <Header containerClass="pt-6" displayLogos={true} />
         <div className="flex-auto flex items-center">
           {children}
