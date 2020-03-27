3f94eb1f09309c8081f60ce03183c069431fc4b9
Brian <brian.ludlam@gmail.com>
2020-03-27 16:12:32 -0400

    Remove unused components, replace by core components

--- src/components/PublicProduct/utils.js
+++
@@ -1,9 +0,0 @@
-export const getLotStateField = (lot, state, field) => {
-  const details = !!lot.details && lot.details.find(one => one.state === state);
-  const value = (!details || !details.data || !details.data[field]) ? null : details.data[field]
-  return value;
-}
-
-export default {
-	getLotStateField
-}
