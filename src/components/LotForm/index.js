b015a614c820ed75017ac907f734edfd8b475b01
Brian <brian.ludlam@gmail.com>
2020-04-07 19:17:37 -0400

    Move lot inflate and deflate functions to utils

--- src/components/LotForm/index.js
+++ src/components/LotForm/index.js
@@ -1,90 +1,15 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import Button from '../../core/src/components/Elements/Button';
-
-import { genLotID } from '../../services/traceFirebase';
 import MultiForm from '../../core/src/components/MultiForm';
-import { combineFormDataStructures }  from '../../core/src/components/MultiForm/util/field-helpers';
 import unverifiedLot from '../../configs/multiform/unverifiedLot';
-//import FileUpload from '../FileUpload';
-//import ImageCropper from '../ImageCropper';
+import { genLotID } from '../../services/traceFirebase';
+import { inflateLot, deflateLot } from './utils';
 
-const inflateLot = (data) => {
-  const lot = {}
-  if (!data) return null;
-
-  Object.keys(data).filter((each) => !!each && !!data[each]).forEach((key) => {
-    const keyParts = key.split('-')
-    const [lotField, cat, entry] = keyParts 
-    const value = data[key] ? data[key] : ''
-    if (!value || !lotField || !cat || !entry) return
-    if (lotField === 'parentLot' && !lot.parentLot) lot.parentLot = {};
-    const lotRef = (lotField === 'lot') ? lot : lot.parentLot
-
-    if (cat === 'lot') {
-      lotRef[entry] = value
-
-    } else if (cat === 'org') {
-      if (!lotRef.organization) lotRef.organization = {}
-      lotRef.organization[entry] = value
-
-    } else {
-      const state = cat;
-      if (state === 'harvest') {
-        lotRef.state = 'harvest';
-        if (!lotRef.stateDetails) lotRef.stateDetails = { state, data: {} }
-        lotRef.stateDetails.data[entry] = value
-      }
-      if (!lotRef.details) lotRef.details = {}
-      if (!lotRef.details[state]) lotRef.details[state] = { state, data: {} }
-      lotRef.details[state].data[entry] = value
-    }
-  })
-
-  if (!!lot.details) lot.details = Object.values(lot.details);
-  if (!!lot.parentLot?.details) lot.parentLot.details = Object.values(lot.parentLot.details)
-
-  return (Object.keys(lot).length > 0) ? lot : null;
-}
-
-const deflateLot = (lot) => {
-  const parts = {};
-  if (!lot) return parts;
-
-  if (!!lot.name) parts['lot-lot-name'] = lot.name;
-  if (!!lot.totalSupply) parts['lot-lot-totalSupply'] = lot.totalSupply;
-  if (!!lot.forSale) parts['lot-lot-forSale'] = lot.forSale;
-  //if (!!lot.address) parts['lot-lot-address'] = lot.address;
-  if (!!lot.organization?.name) parts['lot-org-name'] = lot.organization.name;
-  if (!!lot.details?.length) {
-    lot.details.forEach((detail) => {
-      if (!detail.state || !detail.data) return;
-      Object.keys(detail.data).forEach((key) => {
-        if (!!detail.data[key]) parts[`lot-${detail.state}-${key}`] = detail.data[key];
-      })
-    })
-  }
-  /*
-  if (!!lot.parentLot?.name) parts['parentLot-lot-name'] = lot.parentLot.name;
-  if (!!lot.parentLot?.address) parts['parentLot-lot-address'] = lot.parentLot.address;
-  if (!!lot.parentLot?.organization?.name) parts['parentLot-org-name'] = lot.parentLot.organization.name;
-  if (!!lot.parentLot?.details?.length) {
-    lot.parentLot.details.forEach((detail) => {
-      if (!detail.state || !detail.data) return;
-      Object.keys(detail.data).forEach((key) => {
-        if (!!detail.data[key]) parts[`parentLot-${detail.state}-${key}`] = detail.data[key];
-      })
-    })
-  }
-  */
-  return parts;
-}
 
 class LotForm extends PureComponent {
 
   constructor(props) {
     super(props);
-    //console.log('CONST PROPS ', props)
     const { populateFromID, lots } = props;
     const populateLot = (!!populateFromID) ? lots.find(lot => populateFromID === lot.id) : null;
 
