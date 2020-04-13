abdd19b1eebc4063197ad7ef1f24b34c928e95ed
Brian <brian.ludlam@gmail.com>
2020-04-13 11:27:51 -0400

    Remove components that were moved to core, and update product profile form.

--- src/components/ProductProfileForm/index.js
+++ src/components/ProductProfileForm/index.js
@@ -7,9 +7,9 @@ import SelectDropdownInput from '../../core/src/components/MultiForm/parts/Selec
 import USStates from '../../core/src/components/MultiForm/constants/USStates';
 import InputWrapper from '../../core/src/components/MultiForm/parts/InputWrapper';
 import { genProductID } from '../../services/traceFirebase';
-import FileUpload from '../FileUpload'
+import FileUpload from '../../core/src/components/Forms/FileUpload';
 import QRCodeView from '../QRCodeView';
-import ImageCropper from '../ImageCropper';
+import ImageCropper from '../../core/src/components/Forms/ImageCropper';
 import LotDetailSelector from './components/LotDetailSelector';
 import CertificationCheckboxes from './components/CertificationCheckboxes';
 import PackagingDatePicker from './components/PackagingDatePicker';
