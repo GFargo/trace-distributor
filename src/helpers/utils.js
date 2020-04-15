efdc72b25b7ba1b3f84aa46636ce9689870b1b38
Brian <brian.ludlam@gmail.com>
2020-04-15 10:13:41 -0400

    Refactor and doc cleanObjectProps util
    Good call, this needed to be fixed anyway. It was deleting zeros and falses too, which are legit. Doc'd now and tested. Let me know if looks good.

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,20 +1,26 @@
-
-
+/**
+ * Returns a recursively cleaned object, with deleted: nulls, empty objects, 
+ * undefineds, empty arrays, and empty strings.
+ *
+ * @export
+ * @param {*} obj
+ * @returns new appended obj.
+ */
 export const cleanObjectProps = (o) => {
-  //console.log('firebase cleanProductFields, object: ', obj);
   if (!o || typeof o !== 'object') return o;
   const obj = {...o}; // TODO replace with deep copy function if needed
   Object.keys(obj).forEach((key) => {
     const objType = typeof obj[key];
-    if (!obj[key]) {
-      delete obj[key]
-    } else if (objType === 'object') {
-      cleanObjectProps(obj[key])
-      if (!Object.keys(obj[key]).length) {
-        delete obj[key]
+    if (objType === 'object') {
+      if (!obj[key] || !Object.keys(obj[key]).length) {
+        delete obj[key];// delete null or empty objects
+      } else {
+        cleanObjectProps(obj[key]);// recurse into object
       }
+    } else if (!obj[key] && (objType === 'string' || objType === 'undefined')) {
+      delete obj[key];// delete empty strings and undefined
     } else if (objType === 'array' && !obj[key].length) {
-      delete obj[key]
+      delete obj[key];// delete empty arrays
     }
   })
   return obj;
