c4a1e7805b2cacb3ffc99c2cccb8b83311167760
Brian <brian.ludlam@gmail.com>
2020-04-16 08:01:50 -0400

    Update IPFS env vars

--- src/services/traceIPFS.js
+++ src/services/traceIPFS.js
@@ -1,7 +1,23 @@
-const ipfsClient = require('ipfs-http-client');
-const ipfs = new ipfsClient({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
+const IPFS_SERVER =
+  process.env.REACT_APP_IPFS_SERVER ||
+  'ipfs.infura.io';
 
-const filePath = file => `trace-data/${file.id}`
+const IPFS_PORT =
+  process.env.REACT_APP_IPFS_PORT ||
+  5001;
+
+const IPFS_PROTOCOL =
+  process.env.REACT_APP_IPFS_PROTOCOL ||
+  'https';
+
+const IPFS_PATH =
+  process.env.REACT_APP_IPFS_PATH ||
+  'trace-data/';
+
+const ipfsClient = require('ipfs-http-client');
+const ipfs = new ipfsClient({ host: IPFS_SERVER, port: IPFS_PORT, protocol: IPFS_PROTOCOL });
+
+const filePath = file => `${IPFS_PATH}${file.id}`
 const fileContent = file => ipfsClient.Buffer.from(JSON.stringify(file))
 
 export const ipfsAddFile = async (file, onlyHash) => {
